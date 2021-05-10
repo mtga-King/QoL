af9750e3e8fe04adfff1ed13eb5dd511bc2706f8
aclengen <aclengen@gmail.com>
2021-05-10 11:52:49 -0400

    5/10/21
    * Updated Logs to not Spam, hopefully
    
    Going to try and clean up code

--- src/QOLCache.js
+++ src/QOLCache.js
@@ -3,7 +3,7 @@ exports.mod = (mod_info) => {
 
 	//Load Settings from Config.json
 	const config = require("../config.js");
-	const traders = ["5ac3b934156ae10c4430e83c.json", "579dc571d53a0658a154fbec.json", "nugent.json"]
+	const traders = ["5ac3b934156ae10c4430e83c.json", "579dc571d53a0658a154fbec.json", "nugent.json"];
 
 	let locationfile = fileIO.readParsed(db.user.cache.locations); //var to store cache.locations.json
 	let itemfile = fileIO.readParsed(db.user.cache.items); //var to store cache.items.json
@@ -41,7 +41,6 @@ exports.mod = (mod_info) => {
 		for (let exf in locationfile[map].base.exits){ //store base/exits parameters
 				if (config.ExfilTimer.EditExfilTimer == true){
 
-					logger.logInfo(`   [MOD] -- Exfil Timers: Editable`);
 					if (config.ExfilTimer.RealisticExfilTimers == true){
 						if (locationfile[map].base.exits[exf].ExfiltrationTime >= 5 && locationfile[map].base.exits[exf].ExfiltrationTime <= 10){
 							locationfile[map].base.exits[exf].ExfiltrationTime = 5;
@@ -49,42 +48,52 @@ exports.mod = (mod_info) => {
 						if (locationfile[map].base.exits[exf].ExfiltrationTime >= 15 && locationfile[map].base.exits[exf].ExfiltrationTime <= 60){
 							locationfile[map].base.exits[exf].ExfiltrationTime = 15;
 						} 
-						logger.logInfo(`   [MOD] -- Realistic Exfil Timers: Set`);
 					} else {
 						//find ExfiltrationTime and change it to NewExfiltrationTimerTime
 						locationfile[map].base.exits[exf].ExfiltrationTime = config.ExfilTimer.NewExfilTimerTime;
-						logger.logInfo(`   [MOD] -- User Set Exfil Timers: Set`);
 					}
 					
 				}
 				if (config.UnlockExfils.UnlockExfiltrationType == true){
 					if (locationfile[map].base.exits[exf].PassageRequirement !== "WorldEvent"){ //if ExfilType is not WorldEvent then
 						locationfile[map].base.exits[exf].ExfiltrationType = "Individual";
-
-						logger.logInfo(`   [MOD] -- Exfils: Unlocked`);
 					}
-				}else {
-					logger.logInfo(`   [MOD] -- Exfils: Default`);
-
+				}
 				if (config.RemovePassageRequirement == true || locationfile[map].base.exits[exf].PassageRequirement !== "WorldEvent"){
 					//Edit and Empty Exfil Item Requirements
 							locationfile[map].base.exits[exf].Id = "";
 							locationfile[map].base.exits[exf].PassageRequirement = "None"
 							locationfile[map].base.exits[exf].Count = 0;
 							locationfile[map].base.exits[exf].RequirementTip = "";
-
-							logger.logInfo(`   [MOD] -- Passages: Unlocked`);
 					}					
-				}
 				//Chance for Exfil to be Open
 				if (config.UnlockExfils.AlwaysExfil.EditExfilChance == true){
 					//Edit Chance for Extract to be Open
 					locationfile[map].base.exits[exf].Change = config.UnlockExfils.AlwaysExfil.NewExfilChancePercentage
-
-					logger.logInfo(`   [MOD] -- Passages: Unlocked`);
 				}
+			}
 		}
-	}
+
+	//Loop Checks
+	if (config.ExfilTimer.EditExfilTimer == true){
+		logger.logInfo(`   [MOD] -- Exfil Timers: Editable`);
+		if (config.ExfilTimer.RealisticExfilTimers == true){
+			logger.logInfo(`   [MOD] -- Realistic Exfil Timers: Set`);
+		} else 
+			logger.logInfo(`   [MOD] -- User Set Exfil Timers: Set`);
+		}
+		if (config.UnlockExfils.UnlockExfiltrationType == true){
+			if (locationfile[map].base.exits[exf].PassageRequirement !== "WorldEvent"){
+				logger.logInfo(`   [MOD] -- Exfils: Unlocked`);
+			}
+		}else {
+			logger.logInfo(`   [MOD] -- Exfils: Default`);
+		}
+		if (config.RemovePassageRequirement == true || locationfile[map].base.exits[exf].PassageRequirement !== "WorldEvent"){
+			logger.logInfo(`   [MOD] -- Passages: Unlocked`);
+		}					
+
+
 
 	//Item.json Loop
 	for (let ez in itemfile.data) { //Remove Item Nodes to Access _props
@@ -100,9 +109,6 @@ exports.mod = (mod_info) => {
 								|| itemData === "5448e53e4bdc2d60728b4567") {
 			propsData.Grids[0]._props.filters = []; //0 = 1
 			}
-			logger.logInfo(`   [MOD] -- Container Restrictions: Removed`);
-		} else {
-			logger.logInfo(`   [MOD] -- Container Restrictions: Default`);
 		}
 		
 			
@@ -110,25 +116,36 @@ exports.mod = (mod_info) => {
 		if (config.UnblockFoldables == true) {
 			if(propsData.BlocksFolding == true){
 				propsData.BlocksFolding = false;
-
-				logger.logInfo(`   [MOD] --  Weapon Folding: Unblocked`);
 			}
-		} else{
-			logger.logInfo(`   [MOD] -- Weapon Folding: Default`);
 		}
 			
 		//Remove BlocksArmorVest parameters
 		if (config.UnblockArmorVest == true){
 			if (propsData.BlocksArmorVest == true){
 				propsData.BlocksArmorVest = false;
-
-				logger.logInfo(`   [MOD] -- Armor Stacking: Unlocked`);
 			}
-		} else {
-			logger.logInfo(`   [MOD] -- Armor Stacking: Default`);
 		}
-			
 	}
+
+	//Item Loop Checks
+
+	if (config.RemoveContainerRestrictions == true){
+		logger.logInfo(`   [MOD] -- Container Restrictions: Removed`);
+	} else {
+		logger.logInfo(`   [MOD] -- Container Restrictions: Default`);
+	}
+	if (config.UnblockFoldables == true) {
+		logger.logInfo(`   [MOD] --  Weapon Folding: Unblocked`);
+	} else{
+		logger.logInfo(`   [MOD] -- Weapon Folding: Default`);
+	}
+	if (config.UnblockArmorVest == true){
+		logger.logInfo(`   [MOD] -- Armor Stacking: Unlocked`);
+	} else {
+		logger.logInfo(`   [MOD] -- Armor Stacking: Default`);
+	}
+
+
 	
 	//ExfiltrationType = Individual, SharedTimer, Manual
 	//PassageRequirement = None, Empty, TransferItem, WorldEvent, ScavCooperation
